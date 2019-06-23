5e557978d11bdb292781980a83aa5528d05bd08d
Mohale <mjmolieleng@gmail.com>
2019-06-23 16:51:29 +0200

    added form array and it's data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'form-array-example';
+
+  skillsArray: FormArray;
+  profileForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+
+    this.profileForm = this.formBuilder.group({
+      customerName: '',
+      email: '',
+      skills: this.formBuilder.array([this.createSkill()])
+    });
+
+    this.createSkill();
+  }
+
+  createSkill(): FormGroup {
+    return this.formBuilder.group({
+      name: '',
+      description: '',
+      price: ''
+    });
+  }
+
+  addSkill() {
+    this.skillsArray = this.profileForm.get('skills') as FormArray;
+    this.skillsArray.push(this.createSkill());
+    console.log(this.skillsArray.length)
+    this.profileForm.controls.skills = this.skillsArray;
+  }
+
+  getFormArray(): FormArray {
+    return <FormArray>this.profileForm.controls.skills;
+  }
 }
